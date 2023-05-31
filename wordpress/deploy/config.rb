fa86b1ebd284885e187acc4ea3b8c3bbdb4d3ee6
Julien Minguely <jminguely@gmail.com>
2023-05-31 16:26:32 +0200

    Prepare CI

--- wordpress/deploy/config.rb
+++
@@ -1,8 +0,0 @@
-set :repo_url, 'git@github.com:jminguely/ecolecouture.ch.git'
-
-set :branch, :main
-
-set :log_level, :info
-
-set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
-set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
